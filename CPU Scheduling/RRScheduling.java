fcff4035a3c2149c9540d3443301da7b6493e371
manas2025 <106134826+manas2025@users.noreply.github.com>
2022-11-20 16:49:17 +0530

    Update RRScheduling.java

--- CPU Scheduling/RRScheduling.java
+++ CPU Scheduling/RRScheduling.java
@@ -1,87 +1,81 @@
 import java.util.*;
 
+class Process {
+    int processID;
+    int arrival, burst, waiting, turnAround, remainingTime;
+    int finish,completionTime;
+}
 public class RRScheduling {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int quantum, sum = 0;
+    public static void main(String[] args) {
+        int n,sumBurst=0,quantum,time;
+        double avgWAT=0,avgTAT=0;
+        Scanner sc=new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
         System.out.println("*** RR Scheduling (Preemptive) ***");
         System.out.print("Enter Number of Process: ");
-        int i,n = sc.nextInt();
-        int sumBurst=0;
-        int time;
-        int burstTime[] = new int[n];
-        int arrivaltime[] = new int[n];
-        int waitingTime[] = new int[n];
-        int TAT[] = new int[n];
-        int completionTime[] = new int[n];
-        int remburstTime[] = new int[n];
-        double avgWT=0;
-        double avgTAT=0;
-        Queue<Integer> q=new LinkedList<>();
-        for ( i = 0; i < n; i++) {
-            System.out.print("\nEnter Arrival Time for processor " + (i+1) + ":");
-            arrivaltime[i] =  sc.nextInt();
-            System.out.print("Enter Burst Time for processor " + (i + 1) + " : ");
-            burstTime[i] = sc.nextInt();
-            remburstTime[i]=burstTime[i];
-            completionTime[i]=0;
-            sumBurst+=burstTime[i];
-            
+        n=sc.nextInt();
+        Process[] p = new Process[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = new Process();
+            p[i].processID = i + 1;
+            System.out.print("Enter the arrival time for P" + (i + 1) + ": ");
+            p[i].arrival = sc.nextInt();
+            System.out.print("Enter the burst time for P" + (i + 1) + ": ");
+            p[i].burst = sc.nextInt();
+            p[i].remainingTime = p[i].burst;
+            p[i].finish = 0;
+            sumBurst += p[i].burst;
+            System.out.println();
         }
-        System.out.print("\n\nEnter Time quantum:");
+        System.out.print("\nEnter time quantum: ");
         quantum = sc.nextInt();
+        Process pTemp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (p[i].arrival > p[j].arrival) {
+                    pTemp = p[i];
+                    p[i] = p[j];
+                    p[j] = pTemp;
+                }
+            }
+        }
         q.add(0);
-        for (i = 0; i < n-1; i++) {
-           if(arrivaltime[i]>arrivaltime[i+1]){
-                arrivaltime[i+1]=arrivaltime[i];
-                burstTime[i+1]=burstTime[i];
-                completionTime[i+1]=completionTime[i];
-                waitingTime[i+1]=waitingTime[i];
-                TAT[i+1]=TAT[i];
-                remburstTime[i+1]=remburstTime[i];
-           }
-        }
-        for(time=arrivaltime[0];time<sumBurst;){
-            Integer I=q.remove();
-            i=I.intValue();
-            if(remburstTime[i]<=quantum){
-                time+=remburstTime[i];
-                remburstTime[i]=0;
-                completionTime[i]=1;
-                waitingTime[i]=time-arrivaltime[i]-burstTime[i];
-                TAT[i]=time-arrivaltime[i];
-            
-                for(int j=0;j<n;j++){
-                    Integer J=Integer.valueOf(j);
-                    if((arrivaltime[j]<=time)&&(completionTime[j]!=1) && (!q.contains(J))){
-                    q.add(J);
-                    }
-                }
-            }
-            else{
-                time+=quantum;
-                remburstTime[i]-=quantum;
-                for(int j=0;j<n;j++){
-                    Integer J=Integer.valueOf(j);
-                    if((arrivaltime[j]<=time)&&(completionTime[j]!=1) && (i!=j) && (!q.contains(J))){
+        for (time = p[0].arrival; time < sumBurst;) {
+            Integer I = q.remove();
+            int i = I.intValue();
+            if (p[i].remainingTime <= quantum) {
+                time += p[i].remainingTime;
+                p[i].remainingTime = 0;
+                p[i].finish = 1;
+                p[i].completionTime=time;
+                p[i].waiting = time - p[i].arrival - p[i].burst;
+                p[i].turnAround = time - p[i].arrival;
+                for (int j = 0; j < n; j++) {
+                    Integer J = Integer.valueOf(j);
+                    if ((p[j].arrival <= time) && (p[j].finish != 1) && (!q.contains(J)))
                         q.add(j);
-                    }
-                    q.add(i);
-
                 }
+            } else {
+                time += quantum;
+                p[i].remainingTime -= quantum;
+                for (int j = 0; j < n; j++) {
+                    Integer J = Integer.valueOf(j);
+                    if (p[j].arrival <= time && p[j].finish != 1 && i != j && (!q.contains(J)))
+                        q.add(j);
+                }
+                q.add(i);
             }
         }
-        for( i=0;i<n;i++){
-            avgWT+=waitingTime[i];
-            avgTAT+=TAT[i];
-        }
-        System.out.println("\n*** Priority Scheduling (Non Preemptive) ***");
+        System.out.println("\n*** RR Scheduling (Preemptive) ***");
         System.out.println("Processor\tArrival time\tBrust time\tCompletion Time\t\tTurn around time\tWaiting time");
         System.out.println("----------------------------------------------------------------------------------------------------------");
-        for (i = 0; i < n; i++) {
-            System.out.println("P"+(i+1)+"\t\t"+arrivaltime[i]+"ms\t\t"+burstTime[i]+"ms\t\t"+completionTime[i]+"ms\t\t\t"+TAT[i]+"ms\t\t\t"+waitingTime[i]+"ms");
+        for (int i = 0; i < n; i++) {
+            System.out.println(("P"+1)+"\t\t"+p[i].arrival+"ms\t\t"+p[i].burst+"ms\t\t"+p[i].completionTime+"ms\t\t\t"+p[i].turnAround+"ms\t\t\t"+p[i].waiting+"ms");
+            avgWAT += p[i].waiting;
+            avgTAT += p[i].turnAround;
         }
-        System.out.println("\nAverage waiting time: " + (avgWT / n) + "\nAverage turn around time: " + (avgTAT / n));
-    }
+        System.out.println("\nAverage turn around time of processor: "+(avgTAT/n)+"ms\nAverage waiting time of processor: "+(avgWAT/n)+"ms"); 
+        }
 }
+
